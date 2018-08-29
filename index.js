68a90ae087efc662d68c9807123a873ff052d32a
Brennan Blair <jbb341@hotmail.com>
2018-08-29 15:01:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length !== 0) {
+    return "Currently serving " + katzDeliLine.shift() + "."
+  } else
+    return "There is nobody waiting to be served!"
+} 
+function currentLine(katzDeliLine) {
+  var lineArray = [`The line is currently: `]
+  for (var i = 0;i < katzDeliLine.length;i++) {
+    lineArray.push([i + 1] + ". " + katzDeliLine[i])
+  }
+ return lineArray.join() 
+} 
